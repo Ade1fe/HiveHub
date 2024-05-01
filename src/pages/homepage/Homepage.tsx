5d25e722dd2964dcb9fd0e49239bd3b01f261049
Damilola Adisa <damilolaadisa@Adisas-MacBook-Pro.local>
2024-05-01 10:35:57 +0100

    created component folder and card folder

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import { Box } from '@chakra-ui/react'
+import { ItemCard } from '../../components'
 
 
 const Homepage = () => {
   return (
     <Box>
       hello world
+      <ItemCard />
     </Box>
   )
 }

06f44c8a9c87b0aee0851b37c9a6209bb3ebadf2
TubiOb <Tubiobaloluwa@gmail.com>
2024-05-01 17:58:01 +0100

    sorted framer motion issue on landing page

--- src/pages/landingpage/LandingPage.tsx
+++ src/pages/landingpage/LandingPage.tsx
@@ -1,9 +1,16 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
-import { motion } from "framer-motion";
-import reader from '../../assets/portrait-young-african-woman-with-laptop-white.png'
+import { Box, Button, Image, keyframes, Text } from "@chakra-ui/react"
+import { AnimatePresence, motion, Transition } from "framer-motion";
+import reader from '../../assets/woman-using-digital-tablet-technology.png'
 
+const animation = keyframes`
+    initial={{ opacity: 1, y: -20 }}
+    animate={{ opacity: 1, y: 0 }}
+    exit={{ opacity: 1, y: -20 }}
+`
 
 const LandingPage = () => {
+const bounceAnimation = `${animation} infinity 1.5s easeInOut reverse`
+
   return (
     <Box display='flex' flexDir='column' w='100%' alignItems='center' justifyContent='center' gap='50px'>
       <Box display='flex' flexDir='row' w='100%' h='auto' alignItems='center' justifyContent='space-between' textAlign='center' px='40px' py='12px' borderBottom='1px' borderBottomColor='lightgray'>
@@ -14,7 +21,7 @@ const LandingPage = () => {
         </Box>
       </Box>
 
-      <Box display='flex' flexDir='row' w='95%' marginX='auto' alignItems='center' justifyContent='space-between' px={['', '50px']} py={['', '90px']} gap='60px'>
+      <Box display='flex' flexDir='row' w='95%' marginX='auto' alignItems='center' justifyContent='space-between' px={['', '3%']} py={['', '7%']} gap='60px'>
         <Box boxSize='75%' display='flex' flexDir='column' gap='10px'>
           <Text as='h3' fontSize='58px' fontWeight='600' lineHeight='50px' wordBreak='keep-all'>
             Embark on an Adventure
@@ -22,11 +29,15 @@ const LandingPage = () => {
           <Text as='p' fontSize='larger' fontWeight='500' textColor='GrayText'>
             Dive into Stories, Discover Insights, and Get Entertained!
           </Text>
+
+          <Button w='150px' px='10px' borderRadius='250px' variant='solid' bg='black' color='white' colorScheme='black' fontWeight='400' cursor='pointer'>Start reading</Button>
         </Box>
 
-        <Box as={motion.div} animate={{ y: [-10, 10, -10] }} transition='1.5s infinte' boxSize='xl' h='auto' alignItems='center' justifyContent='center' >
-          <Image src={reader} objectFit='cover' alt='reader' />
-        </Box>
+        <AnimatePresence>
+          <motion.div initial={{ opacity: 1, y: -20 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 1, y: -20 }} transition={{ duration: 3.5, repeat: Infinity, repeatType: 'reverse', ease: 'easeInOut' }} className="stupidChakraUI" >
+            <Image src={reader} objectFit='cover' alt='reader' />
+          </motion.div>
+        </AnimatePresence>
         
         <Box></Box>
       </Box>

cce1412b00b08dac8a8b3974c121e96e4b6f8120
Damilola Adisa <damilolaadisa@Adisas-MacBook-Pro.local>
2024-05-02 21:27:38 +0100

    added footer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 export {default as ItemCard} from './cards/ItemCard';
+export {default as Footer} from './Footer';
 export {default as SideContainer}from './SideContainer';
 export {default as TabContainer}  from './tabs/TabContainer';
 export {default as AdvertContainer} from './adverts/AdvertContainer';
-// export {default as SideContainer} from './adverts/AdvertContainer';
+// export {default as Footer} from './adverts/AdvertContainer';

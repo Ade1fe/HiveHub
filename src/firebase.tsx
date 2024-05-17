249640d4a579dbf90db798cb0fac82d7074c21b3
Damilola Adisa <damilolaadisa@Adisas-MacBook-Pro.local>
2024-05-17 18:23:25 +0100

    cleaned up firebase.tsx

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,8 +1,10 @@
+
 import { initializeApp } from "firebase/app";
-import { getStorage } from 'firebase/storage';
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, GoogleAuthProvider, FacebookAuthProvider, onAuthStateChanged } from 'firebase/auth'
 import { getDatabase } from 'firebase/database'
+import { getAuth, GoogleAuthProvider, FacebookAuthProvider, onAuthStateChanged } from 'firebase/auth'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDi1RTxULn0ES75-c_BckdG6XxkcLugf_Y",
@@ -14,55 +16,22 @@ const firebaseConfig = {
   measurementId: "G-K3LQWPQE16"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const firestore = getFirestore(app);
-export const database = getDatabase(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app);
 export const GoogleUser = new GoogleAuthProvider();
 export const FacebookUser = new FacebookAuthProvider();
+export const database = getDatabase(app);
 
+// Listen for changes in authentication state
 onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log('User is logged in:', user.uid);
-  } else {
-    console.log('User is logged out');
-  }
-});
+    if (user) {
+      console.log('User is logged in:', user.uid);
+    } else {
+      console.log('User is logged out');
+    }
+  });
 
-// // firebase.ts
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import { getFirestore } from "firebase/firestore";
-// import { getAuth, onAuthStateChanged } from "firebase/auth";
-// import { getStorage } from "firebase/storage";
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCemOs_j_yNCeEmTPE3bYWnUfhfW-e7Oeo",
-//     authDomain: "fir-fin-3e351.firebaseapp.com",
-//     projectId: "fir-fin-3e351",
-//     storageBucket: "fir-fin-3e351.appspot.com",
-//     messagingSenderId: "444934139311",
-//     appId: "1:444934139311:web:867df21096c53d4f25535a"
-//   };
-  
-  
-
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const auth = getAuth(app);
-// const firestore = getFirestore(app);
-// const storage = getStorage(app);
-
-// // Listen for changes in authentication state
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//       console.log('User is logged in:', user.uid);
-//     } else {
-//       console.log('User is logged out');
-//     }
-//   });
-
-// export { app, analytics, auth, firestore, storage, onAuthStateChanged };
+export { app, analytics, auth, firestore, storage, onAuthStateChanged };
